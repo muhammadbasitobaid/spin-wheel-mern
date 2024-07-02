c118f41daf9061489730afbab4960a2468581a8a
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2024-07-03 04:15:11 +0500

    formatted the reducer file

--- client/src/store/reducers/wheel.ts
+++ client/src/store/reducers/wheel.ts
@@ -1,25 +1,32 @@
-import { 
-  SET_THEME, 
-  SET_VOLUME, 
-  SET_HISTORY, 
-  SET_SELECTED_WHEEL, 
-  SET_INPUT_NUMBERS, 
-  SET_ACTIVE_MODAL, 
-  SET_RESULT, 
-  SET_SPINNING_SPEED_LEVEL, 
-  SET_SPINNING_DURATION, 
-  SET_MANUALLY_STOP_OPTION, 
-  SET_RANDOM_INITIAL_ANGLE_OPTION, 
-  SET_MYSTERY_SPIN_OPTION, 
+import {
+  SET_THEME,
+  SET_VOLUME,
+  SET_HISTORY,
+  SET_SELECTED_WHEEL,
+  SET_INPUT_NUMBERS,
+  SET_ACTIVE_MODAL,
+  SET_RESULT,
+  SET_SPINNING_SPEED_LEVEL,
+  SET_SPINNING_DURATION,
+  SET_MANUALLY_STOP_OPTION,
+  SET_RANDOM_INITIAL_ANGLE_OPTION,
+  SET_MYSTERY_SPIN_OPTION,
   SET_SPIN_COUNT_OPTION,
   SET_CONFETTI,
   SET_SOUND,
   SET_CONFETTI_TYPE,
   SET_SOUND_TYPE,
-  WheelActions, 
+  WheelActions,
 } from "../actions/wheel";
 
-import { THEMES, Wheel, WheelItem, wheels, defaultSpinConfig, SpinConfig } from "src/constants";
+import {
+  THEMES,
+  Wheel,
+  WheelItem,
+  wheels,
+  defaultSpinConfig,
+  SpinConfig,
+} from "src/constants";
 import { ModalNames } from "src/pages/HomePage";
 
 export interface WheelState {
@@ -48,14 +55,21 @@ export const initialState: WheelState = {
 
 type ActionTypes = WheelActions;
 
-const wheelReducer = (state = initialState, action: ActionTypes): WheelState => {
+const wheelReducer = (
+  state = initialState,
+  action: ActionTypes
+): WheelState => {
   switch (action.type) {
     case SET_VOLUME:
       return { ...state, volume: action.payload };
     case SET_HISTORY:
       return { ...state, history: action.payload };
     case SET_SELECTED_WHEEL:
-      return { ...state, selectedWheel: action.payload, history: action.payload.options };
+      return {
+        ...state,
+        selectedWheel: action.payload,
+        history: action.payload.options,
+      };
     case SET_INPUT_NUMBERS:
       return { ...state, inputNumbers: action.payload };
     case SET_ACTIVE_MODAL:
@@ -65,54 +79,57 @@ const wheelReducer = (state = initialState, action: ActionTypes): WheelState => 
     case SET_THEME:
       return { ...state, selectedTheme: action.payload };
     case SET_SPINNING_SPEED_LEVEL:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, spinningSpeedLevel: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, spinningSpeedLevel: action.payload },
       };
     case SET_SPINNING_DURATION:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, spinningDuration: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, spinningDuration: action.payload },
       };
     case SET_MANUALLY_STOP_OPTION:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, manuallyStopOption: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, manuallyStopOption: action.payload },
       };
     case SET_RANDOM_INITIAL_ANGLE_OPTION:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, randomInitialAngleOption: action.payload } 
+      return {
+        ...state,
+        spinConfig: {
+          ...state.spinConfig,
+          randomInitialAngleOption: action.payload,
+        },
       };
     case SET_MYSTERY_SPIN_OPTION:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, mysterySpinOption: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, mysterySpinOption: action.payload },
       };
     case SET_SPIN_COUNT_OPTION:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, spinCountOption: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, spinCountOption: action.payload },
       };
     case SET_CONFETTI:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, confetti: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, confetti: action.payload },
       };
     case SET_SOUND:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, sound: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, sound: action.payload },
       };
     case SET_CONFETTI_TYPE:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, confettiType: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, confettiType: action.payload },
       };
     case SET_SOUND_TYPE:
-      return { 
-        ...state, 
-        spinConfig: { ...state.spinConfig, soundType: action.payload } 
+      return {
+        ...state,
+        spinConfig: { ...state.spinConfig, soundType: action.payload },
       };
     default:
       return state;

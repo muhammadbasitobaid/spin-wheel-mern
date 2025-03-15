2da0be8d1de8ce38a77e8d47c52cc34afa5f5055
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2025-03-16 03:19:03 +0500

    removed the user fetching loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Route, Navigate, Routes } from "react-router-dom";
 import { attemptGetUser } from "./store/thunks/user";
 import {
@@ -9,31 +9,19 @@ import {
 import { useAppDispatch } from "./store/hooks";
 import { AuthRoute } from "./components/AuthRoute";
 // import Test from "./pages/Test";
-import Spinner from "./components/common/Spinner";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import AboutUs from "./pages/AboutUs";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import ContactUs from "./pages/ContactUs";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(attemptGetUser())
-      .then(() => {
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+    dispatch(attemptGetUser());
   }, [dispatch]);
 
-  return loading ? (
-    <div className="flex items-center justify-center h-screen ">
-      <Spinner />
-    </div>
-  ) : (
+  return (
     <Routes>
       <Route
         path="/account/confirm/:token"

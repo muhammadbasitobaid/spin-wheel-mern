28b4bf8879bba487fa279f7a29dc25ba1dd05ca4
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2025-03-15 14:03:57 +0500

    added circular controller for volume control and and fixed the site map indexing issue in robots.txt file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { Route, Navigate, Routes } from "react-router-dom";
 import { attemptGetUser } from "./store/thunks/user";
-import { ConfirmPage, 
-  // HealthPage, 
-  HomePage } from "./pages";
+import {
+  ConfirmPage,
+  // HealthPage,
+  HomePage,
+} from "./pages";
 import { useAppDispatch } from "./store/hooks";
 import { AuthRoute } from "./components/AuthRoute";
 // import Test from "./pages/Test";
@@ -42,12 +44,23 @@ export default function App() {
         }
       />
       <Route path="/" element={<HomePage />} />
-      <Route path="/:slug" element={<HomePage />} />
+
+      {/* Explicit wheel routes for better SEO */}
+      <Route path="/yes-or-no-wheel" element={<HomePage />} />
+      <Route path="/random-number-wheel" element={<HomePage />} />
+      <Route path="/random-letter-generator" element={<HomePage />} />
+      <Route path="/random-name-picker" element={<HomePage />} />
+      <Route path="/random-team-generator" element={<HomePage />} />
+
+      {/* Other static routes */}
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/about-us" element={<AboutUs />} />
       <Route path="/contact-us" element={<ContactUs />} />
       <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-      <Route element={<Navigate to="/" replace />} />
+
+      {/* Catch-all route for any other slugs */}
+      <Route path="/:slug" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
--- client/src/components/VolumeController.tsx
+++ client/src/components/VolumeController.tsx
@@ -2,14 +2,19 @@ import { FC, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setVolume } from "src/store/actions/wheel";
 import { RootState } from "src/store/store";
-import { DEFAULT_VOLUME } from "src/constants"
+import { DEFAULT_VOLUME } from "src/constants";
 import { Howler } from "howler";
+import CircularSlider from "@fseehawer/react-circular-slider";
 
 const VolumeController: FC = () => {
-  const { volume = DEFAULT_VOLUME, spinConfig } = useSelector((state: RootState) => state.wheel);
+  const { volume = DEFAULT_VOLUME, spinConfig } = useSelector(
+    (state: RootState) => state.wheel
+  );
+  const MAX_VOLUME = 100;
   const { sound } = spinConfig;
   const dispatch = useDispatch();
   const [isMuted, setIsMuted] = useState<boolean>(volume === 0);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   useEffect(() => {
     // Update Howler global volume when volume or sound state changes
@@ -17,22 +22,118 @@ const VolumeController: FC = () => {
     setIsMuted(volume === 0);
   }, [volume, sound]);
 
+  useEffect(() => {
+    // Add click outside listener
+    const handleClickOutside = (event: MouseEvent) => {
+      const volumeController = document.getElementById("volume-controller");
+      if (
+        volumeController &&
+        !volumeController.contains(event.target as Node)
+      ) {
+        setIsActive(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newVolume = parseInt(event.target.value);
     dispatch(setVolume(newVolume));
     setIsMuted(newVolume === 0); // Set isMuted based on the slider value
   };
 
+  const handleCircularSliderChange = (value: number) => {
+    if (value > MAX_VOLUME) {
+      return;
+    }
+    dispatch(setVolume(value));
+    setIsActive(true); // Set active state when slider is being used
+  };
+
   const handleMuteToggle = () => {
     const newVolume = isMuted ? 100 : 0; // Determine the new volume based on the current mute state
     dispatch(setVolume(newVolume));
     setIsMuted(!isMuted); // Toggle mute state
+    setIsActive(true); // Set active state when mute is toggled
   };
 
   return (
-    <div className="relative  lg:flex lg:items-end">
-      <div className="w-[64px] absolute group inline-block rounded-full shadow-3xl bg-white p-5 leading-none">
-        <button onClick={handleMuteToggle} disabled={!sound}>
+    <div className="relative lg:flex lg:items-end">
+      <div className="w-[64px] absolute group inline-block rounded-full lg:bg-white lg:shadow-3xl lg:p-5 leading-none">
+        {sound && (
+          <>
+            {/* Circular Slider for Small Screens */}
+            <div
+              id="volume-controller"
+              className="block lg:hidden"
+              onMouseDown={() => setIsActive(true)}
+              onFocus={() => setIsActive(true)}
+            >
+              <div
+                className={`relative transition-transform duration-200 ${
+                  isActive ? "scale-110" : "scale-100 shadow-3xl"
+                }`}
+              >
+                <CircularSlider
+                  width={60}
+                  min={0}
+                  max={100}
+                  dataIndex={volume}
+                  renderLabelValue={
+                    <button
+                      onClick={handleMuteToggle}
+                      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50"
+                    >
+                      <img
+                        src={
+                          isMuted
+                            ? "/assets/icons/volume_mute.svg"
+                            : "/assets/icons/volume.svg"
+                        }
+                        alt={isMuted ? "Mute" : "Speaker"}
+                        className="w-6 h-6 aspect-square cursor-pointer"
+                      />
+                    </button>
+                  }
+                  valueFontSize="12px"
+                  labelFontSize="12px"
+                  knobSize={18}
+                  knobColor="#212121"
+                  progressColorFrom="#0089D6"
+                  progressColorTo="#0089D6"
+                  progressSize={8}
+                  trackColor="#eeeeee"
+                  trackSize={4}
+                  onChange={handleCircularSliderChange}
+                />
+              </div>
+            </div>
+
+            {/* Vertical Slider for Larger Screens */}
+            <div className="hidden lg:block">
+              <div className="hidden group-hover:block absolute bottom-full pb-2 w-full">
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={volume.toString()}
+                  onChange={handleSliderChange}
+                  aria-orientation="vertical"
+                  className="appearance-none h-40 w-6 bg-gray-200 rounded-full focus:outline-none ml-[1px]"
+                  id="range-vertical"
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        <button
+          onClick={handleMuteToggle}
+          disabled={!sound}
+          className="lg:block hidden"
+        >
           <img
             src={
               isMuted
@@ -47,20 +148,6 @@ const VolumeController: FC = () => {
             title={!sound ? "Sound disabled" : ""}
           />
         </button>
-        {sound && (
-          <div className="hidden flex justify-between items-center mt-2 group-hover:block absolute bottom-full pb-2 w-full">
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={volume.toString()} // Use the volume prop value directly
-              onChange={handleSliderChange}
-              aria-orientation="vertical"
-              className="appearance-none h-40 w-6 bg-gray-200 rounded-full focus:outline-none ml-[1px]"
-              id="range-vertical"
-            />
-          </div>
-        )}
       </div>
     </div>
   );

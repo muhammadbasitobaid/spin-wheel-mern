b7af25a354b89900efcfdb32b5b04b58775a85c6
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2026-08-22 20:19:40 +0500

    v2: reset codebase to spec sheet and extracted resources
    - Retain SPEC.md as the v2 source of truth.
    - Move all application resources (icons, sounds, images, favicon, logo) into
      a top-level resources/ folder.
    - Delete all v1 source code, config, tests, and workflows.

--- client/src/store/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
-import { RootState, thunkDispatch } from "./store";
-
-const useAppDispatch = () => useDispatch<thunkDispatch>();
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export { useAppDispatch, useAppSelector };
